3826607f6d0b99ea90e32ff722eafa09dea2abfa
AhmetNeoSari <asari4137@gmail.com>
2025-05-15 16:47:41 +0300

    Change Scheduled thread to Async updatePosition() function

--- world-simulator/src/main/java/com/example/worldsimulator/simulator/TargetSimulator.java
+++ world-simulator/src/main/java/com/example/worldsimulator/simulator/TargetSimulator.java
@@ -3,15 +3,18 @@ package com.example.worldsimulator.simulator;
 import com.example.worldsimulator.data.Point;
 import com.example.worldsimulator.data.Target;
 import com.example.worldsimulator.service.TargetPositionPublisher;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 @Component
+@EnableAsync
 public class TargetSimulator{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TargetSimulator.class);
@@ -20,19 +23,22 @@ public class TargetSimulator{
     private TargetPositionPublisher targetPositionPublisher;
     public long start_time = System.currentTimeMillis();
 
-    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.SECONDS)
+//    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.SECONDS)
+    @Async
     public void updatePosition() {
-        long last_time = System.currentTimeMillis();
-        int timeElapsed = (int)((last_time - start_time) / 1000 );
+        while(true) {
+            long last_time = System.currentTimeMillis();
+            int timeElapsed = (int) ((last_time - start_time) / 1000);
 
-        Point point = target.getPoint();
-        double v_y = target.getSpeed() * Math.cos(target.getHeading());
-        double v_x = target.getSpeed() * Math.sin(target.getHeading());
-        point.setX((int) (target.getInitalPointX() + (v_x * timeElapsed)));
-        point.setY((int) (target.getInitialPointY() + (v_y * timeElapsed)));
-        target.setPoint(point);
-        targetPositionPublisher.sendMessage(point);
-        LOGGER.info(target.toString());
+            Point point = target.getPoint();
+            double v_y = target.getSpeed() * Math.cos(target.getHeading());
+            double v_x = target.getSpeed() * Math.sin(target.getHeading());
+            point.setX((int) (target.getInitalPointX() + (v_x * timeElapsed)));
+            point.setY((int) (target.getInitialPointY() + (v_y * timeElapsed)));
+            target.setPoint(point);
+            //        LOGGER.info(target.toString());
+            targetPositionPublisher.sendMessage(point);
+        }
     }
 
 //    @Async
@@ -44,7 +50,12 @@ public class TargetSimulator{
     public TargetSimulator(Target target, TargetPositionPublisher targetPositionPublisher) {
         this.target = target;
         this.targetPositionPublisher = targetPositionPublisher;
+    }
 
+    @PostConstruct
+    public void run()
+    {
+        updatePosition();
     }
 
 }
