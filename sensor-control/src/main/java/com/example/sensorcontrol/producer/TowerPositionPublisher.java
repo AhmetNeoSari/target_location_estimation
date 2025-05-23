f384a162dedf6e96e1d15bad543dd6487de7cfdd
AhmetNeoSari <asari4137@gmail.com>
2025-05-23 14:59:04 +0300

    Add TowerPositionPublisher service class

---
+++ sensor-control/src/main/java/com/example/sensorcontrol/producer/TowerPositionPublisher.java
@@ -0,0 +1,25 @@
+package com.example.sensorcontrol.producer;
+
+import com.example.basedomain.Point;
+import com.example.basedomain.config.Publisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TowerPositionPublisher implements Publisher<Point> {
+
+    private final KafkaTemplate<String, Point> kafkaTemplate;
+
+    public TowerPositionPublisher(KafkaTemplate<String, Point> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Value("${spring.kafka.topic.produce.position.name}")
+    private String outputTopic;
+
+    @Override
+    public void publish(Point data) {
+        kafkaTemplate.send(outputTopic, data);
+    }
+}
