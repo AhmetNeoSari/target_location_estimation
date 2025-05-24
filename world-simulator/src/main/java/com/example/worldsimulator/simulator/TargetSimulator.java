2c511d48084db50af50bbb9e4abe4952b397bcea
AhmetNeoSari <asari4137@gmail.com>
2025-05-24 10:28:25 +0300

    Fix heading issue

--- world-simulator/src/main/java/com/example/worldsimulator/simulator/TargetSimulator.java
+++ world-simulator/src/main/java/com/example/worldsimulator/simulator/TargetSimulator.java
@@ -2,14 +2,12 @@ package com.example.worldsimulator.simulator;
 
 import com.example.worldsimulator.data.Target;
 import com.example.worldsimulator.service.TargetPositionPublisher;
-import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
-import com.example.basedomain.Point;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -19,7 +17,7 @@ public class TargetSimulator{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TargetSimulator.class);
 
-//    private Point point = new Point();
+    private final int headingRad;
     private Target target;
     private TargetPositionPublisher targetPositionPublisher;
     private final AtomicBoolean running = new AtomicBoolean(false); // use for interrupt thread
@@ -44,8 +42,8 @@ public class TargetSimulator{
         long cuurent_time = System.currentTimeMillis();
         int timeElapsed = (int) ((cuurent_time - start_time) / 1000);
 
-        double v_y = target.getSpeed() * Math.cos(target.getHeading());
-        double v_x = target.getSpeed() * Math.sin(target.getHeading());
+        double v_y = target.getSpeed() * Math.cos(headingRad);
+        double v_x = target.getSpeed() * Math.sin(headingRad);
         target.getPoint().setX((int) (target.getInitalPointX() + (v_x * timeElapsed)));
         target.getPoint().setY((int) (target.getInitialPointY() + (v_y * timeElapsed)));
         LOGGER.info(target.getPoint().toString());
@@ -55,6 +53,7 @@ public class TargetSimulator{
     public TargetSimulator(Target target, TargetPositionPublisher targetPositionPublisher) {
         this.target = target;
         this.targetPositionPublisher = targetPositionPublisher;
+        this.headingRad = Math.toRadians(target.getHeading());
     }
 
     @PreDestroy
