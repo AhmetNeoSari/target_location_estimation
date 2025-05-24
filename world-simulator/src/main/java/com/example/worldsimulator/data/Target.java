ee261668db8ce23e93d016f5034adeef7b2643cb
AhmetNeoSari <asari4137@gmail.com>
2025-05-24 10:37:42 +0300

    Add name field to Target class. Update some code

--- world-simulator/src/main/java/com/example/worldsimulator/data/Target.java
+++ world-simulator/src/main/java/com/example/worldsimulator/data/Target.java
@@ -1,12 +1,12 @@
 package com.example.worldsimulator.data;
 
-import com.example.basedomain.Point;
-import com.example.worldsimulator.service.TargetPositionPublisher;
+import com.example.basedomain.dto.Point;
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import org.apache.kafka.common.protocol.types.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -15,30 +15,27 @@ import org.springframework.beans.factory.annotation.Value;
 public class Target {
     private static final Logger LOGGER = LoggerFactory.getLogger(Target.class);
 
+    private int heading;
+    private int initalPointX;
+    private int initialPointY;
+    private double speed;
+    private String name;
     private Point point;
 
-    public Target() {
-        point = new Point();
-    }
-
-
-    @Value("${target.heading}")
-    private Integer  heading;
-
-    @Value("${target.initial.position.X}")
-    private Integer initalPointX;
-
-    @Value("${target.initial.position.Y}")
-    private Integer initialPointY;
-
-    @Value("${target.speed}")
-    private Double speed;
-
-    @PostConstruct
-    public void init() {
-        point.setX(initalPointX);
-        point.setY(initialPointY);
-        this.heading %= 360;
+    @Autowired
+    public Target(@Value("${target.heading}") final int heading,
+                  @Value("${target.initial.position.X}") final int posX,
+                  @Value("${target.initial.position.Y}") final int posY,
+                  @Value("${target.speed}") final double speed,
+                  @Value("${target.name}") final String name
+                  ) {
+        this.heading        = heading;
+        this.initalPointX   = posX;
+        this.initialPointY  = posY;
+        this.speed          = speed;
+        this.name           = name;
+        System.out.println("this.name: " + name);
+        this.point = new Point(posX, posY, name);
     }
 
 }
