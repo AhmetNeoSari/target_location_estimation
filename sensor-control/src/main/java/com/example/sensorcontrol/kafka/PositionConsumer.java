a2d175ea8e86bc7a7083c96765348a8445bfcb88
AhmetNeoSari <asari4137@gmail.com>
2025-05-26 18:43:32 +0300

    Fix offset reset issue. Make sure central doesn't listen

--- sensor-control/src/main/java/com/example/sensorcontrol/kafka/PositionConsumer.java
+++ sensor-control/src/main/java/com/example/sensorcontrol/kafka/PositionConsumer.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 import com.example.basedomain.dto.Point;
 
@@ -22,6 +24,9 @@ public class PositionConsumer {
 
     private SensorTower tower1 ;
     private SensorTower tower2 ;
+
+    @Value("${CentralUnit.name}")
+    private String centralName;
 
     @Autowired
     public PositionConsumer(@Value("${SensorTower1.Position.X}") final int tower1PosX,
@@ -42,37 +47,67 @@ public class PositionConsumer {
         this.targetBearingPositionPublisher = targetBearingPositionPublisher;
     } //constructor
 
-    @KafkaListener(
-            topics = "${spring.kafka.topic.consume.name}",
-            groupId = "${spring.kafka.consumer.group-id.tower-1}"
-    )
-    public void consumerTower1(Point targetPoint)
-    {
-        try {
-            processTower(tower1, targetPoint);
-        } catch (RuntimeException e) {
-            LOGGER.error("Error processing consumerTower1 data", e);
-        }
-    } //consumerTower1 func
-
-    @KafkaListener(
-            topics = "${spring.kafka.topic.consume.name}",
-            groupId = "${spring.kafka.consumer.group-id.tower-2}"
+    @KafkaListener( groupId = "${spring.kafka.consumer.group-id.tower-2}",
+            topicPartitions ={
+                    @TopicPartition(
+                            topic = "${spring.kafka.topic.consume.name}",
+                            partitionOffsets = @PartitionOffset(
+                                    partition = "0",
+                                    initialOffset = "0",
+                                    seekPosition = "END")
+                    )
+            }
     )
     public void consumerTower2(Point targetPoint)
     {
         try {
+            if (targetPoint.getSourceName().equals(centralName))
+                return;
+
+            if (targetPoint.getX() == tower2.getPoint().getX()
+                    && targetPoint.getY() == tower2.getPoint().getY())
+                return;
             processTower(tower2, targetPoint);
+
         } catch (RuntimeException e) {
             LOGGER.error("Error processing consumerTower2 data", e);;
         }
     } //consumerTower2 func
 
+
+    @KafkaListener( groupId = "${spring.kafka.consumer.group-id.tower-1}",
+            topicPartitions ={
+                    @TopicPartition(
+                            topic = "${spring.kafka.topic.consume.name}",
+                            partitionOffsets = @PartitionOffset(
+                                    partition = "0",
+                                    initialOffset = "0",
+                                    seekPosition = "END")
+                    )
+            }
+    )
+    public void consumerTower1(Point targetPoint)
+    {
+        try {
+            if (targetPoint.getSourceName().equals(centralName))
+                return;
+
+            if (targetPoint.getX() == tower1.getPoint().getX()
+                    && targetPoint.getY() == tower1.getPoint().getY())
+                return;
+
+            processTower(tower1, targetPoint);
+
+        } catch (RuntimeException e) {
+            LOGGER.error("Error processing consumerTower1 data", e);
+        }
+    } //consumerTower1 func
+
     private void processTower(SensorTower tower, Point targetPoint) {
         double degree = CalculateBearing.bearing(tower.getPoint(), targetPoint);
         double distance = CalculateBearing.euclidean_distance(tower.getPoint(), targetPoint);
 
-        tower.getBearingPosition().setDegree((int) degree);
+        tower.getBearingPosition().setDegree(degree);
         tower.getBearingPosition().setDistance(distance);
 
         this.towerPositionPublisher.publish(tower.getPoint());
